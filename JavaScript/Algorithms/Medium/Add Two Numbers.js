19729f31c55310552fa27529cabc41cae1d6481e
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-03-11 11:27:40 +0600

    Added new solution.

---
+++ JavaScript/Algorithms/Medium/Add Two Numbers.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function addTwoNumbers(l1, l2) {
+    let dummy = new ListNode(0);
+    let curr = dummy;
+    let carry = 0;
+
+    while (l1 !== null || l2 !== null) {
+        let sum = carry;
+        
+        if (l1 !== null) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+
+        if (l2 !== null) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        carry = Math.floor(sum / 10);
+        curr.next = new ListNode(sum % 10);
+        curr = curr.next;
+    }
+
+    if (carry > 0) {
+        curr.next = new ListNode(carry);
+    }
+
+    return dummy.next;
+}
